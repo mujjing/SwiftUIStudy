382b3304828a45ca36b71ff54636097927405801
田 知勲 <j-jihoon@winas.jp>
2021-09-18 23:31:19 +0900

    first commit

---
+++ SwiftUIStudy/SwiftUIStudy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIStudy
+//
+//  Created by 전지훈 on 2021/09/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("SwiftUI Study")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
