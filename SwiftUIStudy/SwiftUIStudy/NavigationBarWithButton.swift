22e51f77b3b9d2a360bdbf66d84633df049a471c
田 知勲 <j-jihoon@winas.jp>
2022-01-02 22:36:36 +0900

    Navigation Bar 만들기

---
+++ SwiftUIStudy/SwiftUIStudy/NavigationBarWithButton.swift
@@ -0,0 +1,65 @@
+//
+//  NavigationBarWithButton.swift
+//  SwiftUIStudy
+//
+//  Created by 전지훈 on 2022/01/02.
+//
+
+import SwiftUI
+
+struct NavigationBarWithButton: ViewModifier {
+    
+    var title: String = ""
+    
+    func body(content: Content) -> some View {
+        return content
+            .navigationBarItems(
+                leading: Text(title)
+                    .font(.system(size: 24, weight: .bold))
+                    .padding(),
+                trailing: Button(action: {
+                    print("자산추가 버튼 클릭")
+                }, label: {
+                    Image(systemName: "plus")
+                                    Text("자산추가")
+                        .font(.system(size: 12))
+                        .multilineTextAlignment(.center)
+                }
+            )
+                    .accentColor(.black)
+                    .padding(EdgeInsets(top: 2, leading: 4, bottom: 0, trailing: 10))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.black)
+                    )
+        )
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                let appearance = UINavigationBarAppearance()
+                appearance.configureWithTransparentBackground()
+                appearance.backgroundColor = UIColor(white: 1, alpha: 0.5)
+                UINavigationBar.appearance()
+                    .standardAppearance = appearance
+                UINavigationBar.appearance()
+                    .compactAppearance = appearance
+                UINavigationBar.appearance()
+                    .scrollEdgeAppearance = appearance
+            }
+    }
+    
+}
+
+extension View {
+    func navigationBarWithButtonStyle(_ title: String) -> some View {
+        return self.modifier(NavigationBarWithButton(title: title))
+    }
+}
+
+struct NavigationBarWithButton_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            Color.gray.edgesIgnoringSafeArea(.all)
+                .navigationBarWithButtonStyle("내 자산")
+        }
+    }
+}
