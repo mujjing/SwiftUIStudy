788cff7c4c92d5a2f07b5f5a58251f6707c6e346
田 知勲 <j-jihoon@winas.jp>
2022-03-13 15:11:25 +0900

    gridView -> UIKit 에서 CollectionView와 같은 느낌의 뷰

--- SwiftUIStudy/SwiftUIStudy/ContentView.swift
+++ SwiftUIStudy/SwiftUIStudy/ContentView.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("SwiftUI Study")
-            .padding()
+        grid()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct grid : View {
+    var body: some View {
+        GeometryReader { geo in
+            ScrollView (.vertical, showsIndicators: false) {
+                VStack {
+                    ForEach(1..<6) { i in
+                        HStack {
+                            Image("image\(i)").resizable().frame(width: geo.size.width / 2 - 20).cornerRadius(15)
+                            Image("image\(i)").resizable().frame(width: geo.size.width / 2 - 20).cornerRadius(15)
+                        }.padding(.horizontal).frame(height: UIDevice.current.orientation.isLandscape ? 300 : 150)
+                    }
+                }
+            }
+        }
     }
 }
